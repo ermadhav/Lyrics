79d02cfd441734f2753a080ac8509f1cf1884c69
ermadhav <madhavtiwari2024@gmail.com>
2025-04-25 23:07:31 +0530

    Added api key in it

---
+++ lyrics.py
@@ -0,0 +1,15 @@
+import lyricsgenius
+
+api_key = "jTk1u6luBLwPvflNUcYdfqY46YkLeFQLecTT3JjWaafjb-f9bLAt-AkTdJbWZJI5"  # Insert your Genius API key here
+genius = lyricsgenius.Genius(api_key)
+
+name = input("Enter Artist Name: ")
+song_title = input("Enter Song Title: ")
+
+# Search for the song directly using artist and song name
+song = genius.search_song(song_title, name)
+
+if song:
+    print(song.lyrics)
+else:
+    print("Song not found.")
