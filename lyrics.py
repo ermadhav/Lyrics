81ac9b0ff89a919bca815536e972c805dc5459ec
ermadhav <madhavtiwari2024@gmail.com>
2025-04-25 23:56:47 +0530

    Updated

--- lyrics.py
+++ lyrics.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
-import lyricsgenius  # Make sure to install the lyricsgenius library
+import lyricsgenius  
 
 app = Flask(__name__)
 
-# Initialize Genius API client with a valid access token
 genius = lyricsgenius.Genius("jTk1u6luBLwPvflNUcYdfqY46YkLeFQLecTT3JjWaafjb-f9bLAt-AkTdJbWZJI5 ")  # Update with your new token
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,10 +11,8 @@ def index():
         artist_name = request.form['artist']
         song_title = request.form['title']
         
-        # Search for the song lyrics
         song = genius.search_song(song_title, artist_name)
         if song:
-            # Redirect to the lyrics page with artist, title, and lyrics as URL parameters
             return redirect(url_for('lyrics', artist=artist_name, title=song_title, lyrics=song.lyrics))
         else:
             return render_template('index.html', error="Lyrics not found!")
